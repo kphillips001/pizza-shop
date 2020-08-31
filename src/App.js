b7864973ee662ad04dbe078cde2114e2f67a9e82
Kevin <knphillips01@gmail.com>
2020-08-31 15:41:11 -0500

    completed order container

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
      <GlobalStyle />    
      <FoodDialog openFood={openFood} setOpenFood={setOpenFood}/>
      <Navbar />
+     <Order />
      <Banner />
      <Menu setOpenFood={setOpenFood} />
     </>
--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
-import {FoodLabel} from '../Menu/FoodGrid';
+import { FoodLabel } from '../Menu/FoodGrid';
+import { pizzaRed } from '../Styles/colors';
+import { Title } from '../Styles/title'
 
 const Dialog = styled.div `
   width: 500px;
-  height: 500px;
   background-color: white;
   position: fixed;
   top: 75px;
   z-index: 5;
   left: calc(50% - 250px);
+  display: flex;
+  flex-direction: column;
 `
+export const DialogContent = styled.div `
+  overflow: auto;
+  min-height: 100px;
+`
+export const DialogFooter = styled.div `
+  box-shadow: 0px -2px 10px 0px grey;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+`
+export const ConfirmButton = styled(Title) `
+  margin: 10px;
+  color: white;
+  border-radius: 5px;
+  padding: 10px;
+  text-align: center;
+  width: 200px;
+  cursor: pointer; 
+  background-color: ${pizzaRed}
+`
+
 const DialogShadow = styled.div `
   position: fixed;
   height: 100%;
@@ -33,7 +57,6 @@ const DialogBannerName = styled(FoodLabel) `
   padding: 5px 40px;
 `
 
-
 export function FoodDialog({ openFood, setOpenFood }) {
   function close() {
     setOpenFood()
@@ -46,7 +69,12 @@ export function FoodDialog({ openFood, setOpenFood }) {
       <DialogBanner img={openFood.img}>
         <DialogBannerName>{openFood.name}</DialogBannerName>
       </DialogBanner>
-   </Dialog>
+        <DialogFooter>
+          <ConfirmButton>
+            Confirm
+          </ConfirmButton>
+        </DialogFooter>
+    </Dialog>
   </>
     ) : null
   );
